ebed88adcaf1880c6d05d4c6e2547d423d277bc3
vitorgabrieloliveira <169067855+Vitorgabriel043@users.noreply.github.com>
2026-09-17 09:58:32 -0300

    s

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
              afirmacao: [
-          "No início ficou com medo do que essa tecnologia pode fazer. ",
-          "Achou assustador pensar na velocidade na qual a tecnologia está avançando.",
+                    "No início ficou com medo do que essa tecnologia pode fazer. ",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando.",
         ],
                 texto: "Isso é maravilhoso!",
                  afirmacao: [
-          "Quis saber como usar IA no seu dia a dia.",
-          "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
         ], 
             
         ]
